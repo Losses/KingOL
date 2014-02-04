a7041bd5271b3217eff5ae9ec75ad158402ac58d
Kun Wang <ifreedom.cn@gmail.com>
2014-02-05 00:02:25 +0800

    Fix multiple card select.

--- select_card.php
+++ select_card.php
@@ -1,6 +1,28 @@
 <?php
 
+session_start();
 include_once("function.php");
+
+function find_card($id) {
+    $players_count = get_player_count();
+
+    for ($i = 0; $i < $players_count; $i++) {
+        $file_card = k_db_get("channel", "folder", "result/") . $i;
+        if (file_exists($file_card)) {
+            $file_content = file($file_card);
+            if ($file_content[0] == $id) return $i;
+        }
+    }
+
+    return -1;
+}
+
+$idx = find_card($_SESSION["id"]);
+if ($idx != -1) {
+    echo $idx;
+    exit();
+}
+
 $players_count = get_player_count();
 do {
     $file_temp_card = rand(0, $players_count - 1);
